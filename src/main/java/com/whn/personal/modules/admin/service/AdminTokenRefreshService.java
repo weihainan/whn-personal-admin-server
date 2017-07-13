bbdf138ec4c419c4580f780d9a00a2919fef4475
魏海南 <214372@nd.com>
2017-07-13 19:24:08 +0800

    update

---
+++ src/main/java/com/whn/personal/modules/admin/service/AdminTokenRefreshService.java
@@ -0,0 +1,11 @@
+package com.whn.personal.modules.admin.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author weihainan.
+ * @since 0.1 created on 2017/7/13.
+ */
+@Service
+public class AdminTokenRefreshService {
+}
