cdf73bd0ff4776568cc104e114d13a9f4fc19a9d
魏海南 <214372@nd.com>
2017-08-25 15:32:26 +0800

    update

--- src/main/java/com/whn/personal/internal/support/AppContext.java
+++ src/main/java/com/whn/personal/internal/support/AppContext.java
@@ -1,6 +1,7 @@
 package com.whn.personal.internal.support;
 
 import com.whn.personal.modules.admin.domain.Admin;
+import com.whn.waf.common.context.Context;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -15,38 +16,11 @@ import javax.servlet.http.HttpServletRequest;
  * @since 0.1 created on 2017/7/13.
  */
 @Component
-public class AppContext implements ApplicationContextAware {
+public class AppContext extends Context {
 
     private static ApplicationContext applicationContext;
 
-    private AppContext() {
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        initContextHolder(applicationContext);
-    }
-
-    private static void initContextHolder(ApplicationContext context) {
-        AppContext.applicationContext = context;
-    }
-
-    public static Object getBean(String name) {
-        return applicationContext.getBean(name);
-    }
-
-    public static <T> T getBean(String name, Class<T> requiredType) {
-        return applicationContext.getBean(name, requiredType);
-    }
-
-    public static <T> T getBean(Class<T> requiredType) {
-        return applicationContext.getBean(requiredType);
-    }
-
-
-    public HttpServletRequest getRequest() {
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        return attributes.getRequest();
+    public AppContext() {
     }
 
     private final static String KEY_USER_INFO = "user_info"; //当前用户信息
